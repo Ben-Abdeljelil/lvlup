52f30f223722f661bb6d96ebb5f54cdd4c70243e
Aicha Ben Abdeljelil <benabdeljelilaicha@gmail.com>
2022-01-25 19:47:21 +0100

    first commit

---
+++ components/shared/demo-panel/PageLinks.vue
@@ -0,0 +1,52 @@
+<template lang="html">
+    <v-tabs
+        background-color="gray"
+        color="warning"
+        class="ps-tab-list"
+        grow
+        centered
+    >
+        <v-tab>
+            <a href="#" @click.prevent class="tab-item">Hompages</a>
+        </v-tab>
+        <v-tab-item>
+            <div class="tab-content pt-3">
+                <div class="row">
+                    <div
+                        v-for="demo in homepages"
+                        class="col-md-4 col-6"
+                        :key="demo.id"
+                    >
+                        <demo-item :demo="demo" />
+                    </div>
+                </div>
+            </div>
+        </v-tab-item>
+    </v-tabs>
+</template>
+
+<script>
+import DemoItem from '~/components/elements/commons/DemoItem';
+export default {
+    name: 'PageLinks',
+    components: { DemoItem },
+    data() {
+        return {
+            homepages: [
+                {
+                    id: 1,
+                    url: '/'
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.tab-item {
+    font-size: 16px;
+    color: $color-heading;
+    font-weight: 600;
+}
+</style>
---
+++ components/shared/mobile/HeaderMobile.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+    <header class="header header--mobile">
+        <div class="navigation--mobile">
+            <div class="navigation__left">
+                <nuxt-link to="/" class="ps-logo">
+                    <img src="~/static/img/logo_light.png" alt="martfury" />
+                </nuxt-link>
+            </div>
+            <mobile-header-actions />
+        </div>
+    </header>
+</template>
+
+<script>
+import MobileHeaderActions from '~/components/shared/mobile/modules/MobileHeaderActions';
+export default {
+    name: 'HeaderMobile',
+    components: { MobileHeaderActions }
+};
+</script>
+
+<style lang="scss" scoped></style>
